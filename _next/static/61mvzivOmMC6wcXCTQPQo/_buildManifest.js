93a0cef5e815169ffa619ba04be98d599dc9fc69
st3fanjelo <stefankalenderov@gmail.com>
2026-06-08 23:51:36 +0300

    Publish static site for branch-based GitHub Pages

---
+++ _next/static/61mvzivOmMC6wcXCTQPQo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/404":["static/chunks/pages/404-4d692f966a406438.js"],"/_error":["static/chunks/pages/_error-914d3478dc099efc.js"],sortedPages:["/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
